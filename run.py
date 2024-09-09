59c00ead048d593e6a4f10f971ec6ef4c5b85067
David O'Quigley <oquigld2@gmail.com>
2024-09-09 20:57:09 +0000

    Collect sales data for last 5 weeks

--- run.py
+++ run.py
@@ -96,6 +96,20 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entry_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    The last 5 entries for each sandwich and returns the data
+    as a list of lists
+    """
+    sales = SHEET.worksheet('sales')
+   
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
 
 def main():
     """
@@ -108,4 +122,5 @@ def main():
     update_worksheet(new_surplus_data, 'surplus')
 
 print('Welcome to Love Sandwiches Data Automation\n')
-main()
+# main()
+sales_columns = get_last_5_entry_sales()
